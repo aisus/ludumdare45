0a533e4d6bacab50c6808ccfba338737c0c73098
Andrey Belousov <groundhog448@gmail.com>
2019-10-07 17:35:46 +0400

    Main menu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _weaponPrefab;
+        [SerializeField] private Button _startButton;
+
+        private void Awake()
+        {
+            FindObjectOfType<PlayerCharacter>().ActivateWeapon(_weaponPrefab);
+            _startButton.onClick.AddListener(ScenesLoader.Instance.LoadNextLevel);
+        }
+    }
+}
