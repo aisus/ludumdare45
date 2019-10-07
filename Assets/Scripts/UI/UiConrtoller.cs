669e6bff7d08aba01e1d78e0ca683f9a53b665e2
fallcresthero <fallcresthero@gmail.com>
2019-10-07 21:46:23 +0400

    Changed UI prefab

--- Assets/Scripts/UI/UiConrtoller.cs
+++ Assets/Scripts/UI/UiConrtoller.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Common;
+using Utility;
 
 public class UiConrtoller : MonoBehaviour
 {
     [SerializeField] private Image[] _healthIndicators;
     [SerializeField] private GameObject _deathUi;
+    [SerializeField] private GameObject _PauseUi;
+
+    private bool _isPaused;
 
     private int _currentIndex = 0;
 
@@ -20,5 +26,26 @@ public class UiConrtoller : MonoBehaviour
             _healthIndicators[_currentIndex].color = Color.black;
             _deathUi.SetActive(true);
         }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetPause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void SetPause()
+    {
+        _isPaused = !_isPaused;
+        Time.timeScale = _isPaused ? 0 : 1;
+        _PauseUi.SetActive(_isPaused);
+        GameplayManager.Instance.PlaySound(GameAudioManager.SoundType.PausePress);
     }
 }
